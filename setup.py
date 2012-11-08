ba41f1c87abd3a060c7aa179767563e6aed57a88
Igor Serko <igor.serko@gmail.com>
2012-11-08 12:05:55 +0000

    setup fix + filename changed

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@
 
 from distutils.core import setup
 
+with open('LICENSE.md', 'r') as f:
+    licence = f.read().split('\n')
+
 setup(name='jira2fogbugz',
       version='0.1',
       description='Jira to Fogbugz case importer',
       author='Igor Serko',
       author_email='igor.serko@gmail.com',
       url='https://github.com/iserko/jira2fogbugz',
+      package_dir = {'': 'src'},
+      py_modules = ['jira2fogbugz'],
+      licence=licence,
      )
